4cdeabe9efcadfca53aab22287929eaf9e814ebb
jeffersonjrgibellato <jeffersonjrgibellato@gmail.com>
2024-12-09 15:25:51 -0300

    vv

--- src/data/washTypes.js
+++ src/data/washTypes.js
@@ -1,5 +1,5 @@
 export const washTypes = [
-    { id: 'Interno', label: 'Interna R$100.00' },
-    { id: 'Externo', label: 'Externa R$80.00' },
-    { id: 'Interno e Externo', label: 'Interna e Externa $120.00' },
+    { id: 'Interno R$100.00', label: 'Interna R$100.00' },
+    { id: 'Externo R$80.00', label: 'Externa R$80.00' },
+    { id: 'Interno e Externo R$120.00', label: 'Interna e Externa $120.00' },
   ];
